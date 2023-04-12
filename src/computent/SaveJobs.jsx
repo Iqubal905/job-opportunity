4c6b5c5ba0ad3d40b0d2a89ea99e0e93cbc3c870
Iqubal905 <iqubal905@gmail.com>
2023-04-12 12:57:04 +0600

    ok

--- src/computent/SaveJobs.jsx
+++ src/computent/SaveJobs.jsx
@@ -17,14 +17,14 @@ const SaveJobs = ({singleStore}) => {
                 <button className= ' border-solid border-2 border-zinc-300 p-1 mt-2 mr-2 text-sky-500' >{location}</button>
                 <button className='border-solid border-2 border-zinc-300 p-1 mt-2 mr-2 text-sky-500'>{time}</button>
             </div>
-            <div className='flex pt-3'>
+            <div className='md:flex pt-3'>
              <div className='flex'>
             <img className='pr-1' src={icon_location} alt="" />
              <p className=' text-sm text-gray-500'>{address}</p>
              </div>
              <div className='flex'>
-                <img className=' pl-2 pr-1' src={icon_salary} alt="" />
-             <p className='text-sm text-gray-500'>{salary}</p>
+                <img className=' md:pl-2 pr-1' src={icon_salary} alt="" />
+             <p className='text-sm text-gray-500'> Salary :{salary}</p>
              </div>
             </div>
           </div>
